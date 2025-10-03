ab23695e67dff1c4eb033a2027b6e37837e40465
Amruta2805 <amruta.alset@gmail.com>
2025-10-03 14:29:42 +0530

    worked on hero Ai Assistant using python

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "beach": "https://www.youtube.com/watch?v=mpjNh-uGBY4&list=RDmpjNh-uGBY4&start_radio=1",
+    "holiday": "https://www.youtube.com/watch?v=6w67NOaRe-w&list=RD6w67NOaRe-w&start_radio=1",
+    "banno": "https://www.youtube.com/watch?v=MuCfsZk9lbU&list=RDMuCfsZk9lbU&start_radio=1",
+    "heer": "https://www.youtube.com/watch?v=aNcxgxHcGYg&list=RDaNcxgxHcGYg&start_radio=1"
+}
